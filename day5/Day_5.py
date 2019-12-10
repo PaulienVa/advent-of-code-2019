bb06aa02caa1a9b93acd79aad49b036484e17d4b
Paulien van Alst <paulien@openvalue.nl>
2019-12-10 07:20:30 +0100

    [2019] - day 5 part 2 wip

--- day5/Day_5.py
+++ day5/Day_5.py
@@ -26,7 +26,8 @@ def calc(operator, value1, value2):
 
 def ops_code():
     with open('input1.txt') as file:
-        input = 1
+        # input = 1  --> part 1
+        input = 5
         output = 0
         data = file.read()
         program = [int(x) for x in data.split(",")]
@@ -38,6 +39,7 @@ def ops_code():
             operator, mode1, mode2, mode3 = modes(instructions)
 
             if operator == 1 or operator == 2:
+                print("Ops1")
                 parameter1 = program[index + 1]
                 parameter2 = program[index + 2]
                 value1 = value(parameter1, program, mode1)
@@ -49,12 +51,45 @@ def ops_code():
                     program[index + 3] = calc(operator, value1, value2)
                 index += 4
             elif operator == 3:
+                print("Ops3")
                 program[program[index + 1]] = input
                 index += 2
             elif operator == 4:
+                print("Ops4")
                 output = program[program[index + 1]]
-                index += 2
+                # index += 2
                 print(f"output (1) is {output}")
+                break
+            elif operator == 5:
+                print("Ops5")
+                if mode1 == 0 and program[program[index + 1]] != 0:
+                    index = program[program[index + 2]] if mode2 == 0 else program[index + 2]
+                elif mode1 == 1 and program[index + 1] != 0:
+                    index = program[program[index + 2]] if mode2 == 0 else program[index + 2]
+                else:
+                    index += 3
+            elif operator == 6:
+                print("Ops6")
+                if mode1 == 0 and program[index + 1] == 0:
+                    index = program[program[index + 2]] if mode2 == 0 else program[index + 2]
+                elif mode1 == 1 and program[index + 1] == 0:
+                    index = program[program[index + 2]] if mode2 == 0 else program[index + 2]
+                else:
+                    index += 3
+            elif operator == 7:
+                print("Ops7")
+                if program[index + 1] < program[index + 2]:
+                    program[index + 3] = 1
+                else:
+                    program[index + 3] = 0
+                index += 4
+            elif operator == 8:
+                print("Ops8")
+                if program[index + 1] == program[index + 2]:
+                    program[index + 3] = 1
+                else:
+                    program[index + 3] = 0
+                index += 4
             elif operator == 99:
                 print("Ops 99")
                 break
